ae959826ed074ca7bc46f81be39de006d145ed6c
shocker945516 <shocker945516@gmail.com>
2022-08-02 13:34:10 -0700

    merged

---
+++ src/main/java/com/company_mngm_sys/demo/dao/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.company_mngm_sys.demo.dao;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.company_mngm_sys.demo.entity.Employees;;
+
+public interface EmployeeRepo extends CrudRepository<Employees, Long> {
+
+    @Override
+    public List<Employees> findAll();
+
+}
